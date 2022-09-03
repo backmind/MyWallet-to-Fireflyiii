23b8329c9a7192be8aabbc42e5cbeca95c17ea1b
Yass Fuentes <backmind@gmail.com>
2022-09-04 01:09:42 +0200

    v1.0

---
+++ AccounterExporter.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  2 21:56:45 2022
+
+@author: backm
+"""
+
+import pandas as pd
+import sqlite3
+
+# CONFIG AREA
+DB_PATH = "XX_XX_XXXX_ExpensoDB" # Full path to MyWallet Expense Manager database
+OUTPUT_NAME = "dbexported.csv" # Full path of the exported csv database
+
+
+# DEFINITION AREA
+
+# Read sqlite into dict of pandas DataFrames
+def read_sqlite(dbfile):
+    with sqlite3.connect(dbfile) as dbcon:
+        tables = list(pd.read_sql_query(
+            "SELECT name FROM sqlite_master WHERE type='table';", dbcon)['name'])
+        out = {tbl: pd.read_sql_query(
+            f"SELECT * from {tbl}", dbcon) for tbl in tables}
+
+    return out
+# Query for an account, with fetching its initial founds.
+
+
+def get_account(account):
+        # Funtion to fetch each account in the database
+    # Selecting only account
+    df = pd_db[pd_db["acc_name"] == account]
+    idd = acc[acc["acc_name"] == account].index
+
+    
+    # Fetching and appending initial founds
+    date_init = df.exp_date.min()+pd.Timedelta(days=-1)
+    amount_init = acc.loc[idd, "acc_initial"].values[0]
+    exp_payee_name_init = "Initial founds"
+    category_name_init = "Otro"
+    exp_note_init = "Initial founds"
+    df = df.append(dict(zip(df.columns, [date_init, amount_init, exp_payee_name_init,
+                   account, category_name_init, exp_note_init])), ignore_index=True)
+
+    df.sort_values(by="exp_date", ascending=True, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+def get_account_wtransfers(account):
+    # This function gets the account but also all then transferences involving that account
+    # Selecting only account
+    df = pd_db[pd_db["acc_name"] == account]
+    idd = acc[acc["acc_name"] == account].index
+    dict_idnames = acc["acc_name"].to_dict()
+
+    # Fetching and appending initial founds
+    date_init = df.exp_date.min()+pd.Timedelta(days=-1)
+    amount_init = acc.loc[idd, "acc_initial"].values[0]
+    exp_payee_name_init = "Initial founds"
+    category_name_init = "Other"
+    exp_note_init = "Initial founds"
+    df = df.append(dict(zip(df.columns, [date_init, amount_init, exp_payee_name_init,
+                   account, category_name_init, exp_note_init])), ignore_index=True)
+
+    # Feching and appending transferences
+    # outcomming:
+    out = transfer[transfer.trans_from_id.isin(
+        idd)].reset_index(drop=True)
+    out.trans_from_id.replace(dict_idnames, inplace=True)
+    out.trans_to_id.replace(dict_idnames, inplace=True)
+    out["trans_date"] = pd.to_datetime(out["trans_date"])
+    out["trans_amount"] = out["trans_amount"]*-1
+    out.rename(columns={"trans_date": "exp_date", "trans_amount": "exp_amount", "trans_note": "exp_note",
+                'trans_from_id': "acc_name", 'trans_to_id': "exp_payee_name"}, inplace=True) 
+    out["category_name"] = "Transfer"
+    df = df.append(out)
+
+    # incomming:
+    inc = transfer[transfer.trans_to_id.isin(
+        idd)].reset_index(drop=True)
+    inc.trans_from_id.replace(dict_idnames, inplace=True)
+    inc.trans_to_id.replace(dict_idnames, inplace=True)
+    inc["trans_date"] = pd.to_datetime(inc["trans_date"])
+    inc.rename(columns={"trans_date": "exp_date", "trans_amount": "exp_amount", "trans_note": "exp_note",
+                'trans_from_id': "acc_name", 'trans_to_id': "exp_payee_name"}, inplace=True) 
+    inc["category_name"] = "Transfer"
+    df = df.append(inc)
+
+    df.sort_values(by="exp_date", ascending=True, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+
+# Load of all tables from database:
+
+tbs = read_sqlite(DB_PATH)
+
+tra = tbs["tbl_trans"].set_index("exp_id", drop=True)
+acc = tbs["tbl_account"].set_index("acc_id", drop=True)
+cat = tbs["tbl_cat"].set_index("category_id", drop=True)
+transfer = tbs["tbl_transfer"].set_index("trans_id", drop=True)
+
+pd_db = tra.merge(acc["acc_name"], left_on="exp_acc_id",
+                  right_on="acc_id").drop(labels="exp_acc_id", axis=1)
+pd_db = pd_db.merge(cat["category_name"], left_on="exp_cat",
+                    right_on="category_id").drop(labels="exp_cat", axis=1)
+
+
+# Only actual stuff, not programated one being not paid
+pd_db = pd_db[pd_db.exp_is_paid == 1]
+pd_db["exp_amount"] = pd_db.apply(
+    lambda row: -row.exp_amount if row.exp_is_debit == 0 else row.exp_amount, axis=1)
+pd_db["exp_date"] = pd.to_datetime(pd_db["exp_date"])
+pd_db.sort_values(by="exp_date", ascending=True, inplace=True)
+pd_db.reset_index(drop=True, inplace=True)
+
+
+pd_db = pd_db[["exp_date", "exp_amount", "exp_payee_name",
+               "acc_name", "category_name", "exp_note"]]
+
+
+# Populating a dataframe with all accounts data
+df_acc = pd.DataFrame()
+
+for acc_name in acc["acc_name"]:
+    df_acc = df_acc.append(get_account(acc_name))
+    
+    
+# Feching and appending transferences
+dict_idnames = acc["acc_name"].to_dict()
+df_trn = transfer.reset_index(drop=True)
+df_trn.trans_from_id.replace(dict_idnames, inplace=True)
+df_trn.trans_to_id.replace(dict_idnames, inplace=True)
+df_trn["trans_date"] = pd.to_datetime(df_trn["trans_date"])
+df_trn["trans_amount"] = df_trn["trans_amount"]*-1
+df_trn.rename(columns={"trans_date": "exp_date", "trans_amount": "exp_amount", "trans_note": "exp_note",
+            'trans_from_id': "acc_name", 'trans_to_id': "exp_payee_name"}, inplace=True)    
+df_trn["category_name"] = "Transfer"
+df_acc = df_acc.append(df_trn)  
+
+#trimming multiline notes
+df_acc["exp_note"] = df_acc["exp_note"].str.replace(
+    '\r', ' ').replace('\n', ' ').str.strip()
+
+# Shorting and reindexing dataframe
+df_acc.sort_values(by="exp_date", ascending=True, inplace=True)
+df_acc.reset_index(drop=True, inplace=True)
+
+# Exporting csv
+df_acc.to_csv(OUTPUT_NAME, sep="\t")
--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # MyWallet-to-Fireflyiii
-Simple python processer of the MyWallet expense manager Android app to export its database to Firefly iii
+Python [ETL](https://en.wikipedia.org/wiki/Extract,_transform,_load) processer for exporting [MyWallet expense manager](https://play.google.com/store/apps/details?id=com.apps.balli.mywallet) Android app database to [Firefly iii](https://github.com/firefly-iii/firefly-iii)
+
+# What is the problem?
+Althoug [MyWallet expense manager](https://play.google.com/store/apps/details?id=com.apps.balli.mywallet) is a great budget application, it lacks of advanced statistics and dashboarding, and it is for Android ecosystem only. Also, the syncronization of the data from the app has no cloud compatibility anymore. This forces to a manual creation of raw data backups. Furthermore, this app have no updates since Aug 2016.
+
+Then, [Firefly iii](https://github.com/firefly-iii/firefly-iii) (ffiii) app came to save me: ```"Firefly III" is a (self-hosted) manager for your personal finances. It can help you keep track of your expenses and income, so you can spend less and save more```. My purpose is to ETL data from MyWallet to Firefly iii.
+
+### Dependencies
+
+See *requirements.txt*. You can install through line:
+`pip install -r /path/to/requirements.txt`
+
+>pandas==1.3.5
+
+# Step by step process
+
+## 1. Exporting the MyWallet database
+First, you need to export database:
+1. From the main screen of the app click at "More" on the botton right
+2. Go to "Backup and restore"
+3. Make a new backup by clicking at "Save Backup"
+ A MySQL database file will be saved at SDCard:/MyWallet/Backups in the name of XX_XX_XXXX_ExpensoDB (where XX_XX_XXXX is the current date)
+4. Copy that file in anyplace of your pc
+
+## 2. Transforming database into CSV
+Once you have your database we must make it compatible with firefly-iii. To doing so, we will use the python script of this repository named ```AccounterExporter.py```
+1. Edit the constant ```DB_PATH``` with full filename path to the exported MySQL database
+2. (optional) Edit the constant ```OUTPUT_NAME``` with full filename path to the csv file that will be exported. Otherwise it will be saved as ```./dbexported.csv```
+
+## 3. Load the exported database into Firefly iii
+Once data has been made compatible with Firefly iii you can load it into your Firefly iii instance. To doing so you need:
+1. A running [Firefly iii](https://github.com/firefly-iii/firefly-iii) (ffiii) instance
+2. A running [Firefly iii Data Importer](https://github.com/firefly-iii/data-importer) (FIYI) instance, already connected with your ffiii app.
+
+So in order to import the data, you have to go to your FIYI instance and:
+1. Click on ```Import file```
+2. At ```Importable file``` select the csv we previously exported
+3. At ```Optional configuration file``` select the file ```FIYIcfg\mywallet.json``` wich is part of the current repository
+4. Follow the import steps
+5 ...
+6 Profit!
+
+# And now what?
+Once its done you will have all your data imported at ffiii. There is even some [Android/ios apps](https://docs.firefly-iii.org/firefly-iii/other-pages/3rdparty/#mobile-applications) which can connect with your ffiii instance that can substitude MyWallet with ease.
+
+I do recommend to check [ffiii documentation](https://docs.firefly-iii.org/), wich is quite good and clear.
+
+Its worth to notice that I have included a function into the code called ```get_account_wtransfers()```, which can be handy if you want to play arround with your data from MyWallet, due to it allows you to load a single account with all of its assets (including the ones that came from transferences between accounts)
